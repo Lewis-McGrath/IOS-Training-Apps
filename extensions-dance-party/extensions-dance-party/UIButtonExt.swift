55faae622408dee00ab691162524aec992b04f61
Lewis McGrath <lewismcgrath7@gmail.com>
2017-09-01 16:29:00 +0100

    Initial commit for Extension training App

---
+++ extensions-dance-party/extensions-dance-party/UIButtonExt.swift
@@ -0,0 +1,9 @@
+//
+//  UIButtonExt.swift
+//  extensions-dance-party
+//
+//  Created by Lewis McGrath on 01/09/2017.
+//  Copyright © 2017 Lewis McGrath. All rights reserved.
+//
+
+import Foundation
---
+++ extensions-dance-party/extensions-dance-party/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  extensions-dance-party
+//
+//  Created by Lewis McGrath on 01/09/2017.
+//  Copyright © 2017 Lewis McGrath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var colouriseButton: UIButton!
+    
+    @IBOutlet weak var wiggleButton: UIButton!
+    
+    @IBOutlet weak var dimButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func colourisedButtonWasPressed(_ sender: Any) {
+    }
+    
+
+    @IBAction func wiggleButtonWasPressed(_ sender: Any) {
+    }
+
+    
+    @IBAction func dimButtonWasPressed(_ sender: Any) {
+    }
+    
+}
